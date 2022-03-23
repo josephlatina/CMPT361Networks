689d685fc38f028d7ba823701b696854a7bc0d3d
Joseph Latina <josephlatina93@gmail.com>
2022-03-23 11:56:44 -0600

    Created client.py and server.py with the respective directories

---
+++ client/Client.py
@@ -0,0 +1,13 @@
+# Group Member Names: Joseph Latina, Andrew, Raphelos
+# CMPT361 Project - Secure Mail Transfer Protocol
+
+import json
+import socket
+import os, glob, datetime
+import sys
+
+def client():
+    pass
+
+#----------
+client()
