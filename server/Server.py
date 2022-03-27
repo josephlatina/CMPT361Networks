fbb7ac500d6872b2592c9032c599e076250a69c8
josephlatina <josephlatina93@gmail.com>
2022-03-26 18:24:17 -0600

    finished authenhetication of users

--- server/Server.py
+++ server/Server.py
@@ -5,6 +5,9 @@ import json
 import socket
 import os, glob, datetime
 import sys
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Random import get_random_bytes
 
 def server():
     #server port
@@ -32,19 +35,77 @@ def server():
     while 1:
         #try to connect with current client
         try:
-            #send welcome message and ask for username and pw
+            #Server accepts client connection
+            connectionSocket, addr = serverSocket.accept()
 
-            #authenticate users
+            #Create a fork
+            pid = os.fork()
 
-            #if known client, create fork
+            # 1: For child process, it will handle current client interaction
+            if (pid == 0):
+                #Terminate child process connection to server socket
+                serverSocket.close()
 
-            #for child processes
+                #import server private key
+                with open(os.path.join(sys.path[0], "server_private.pem"), "rb") as f:
+                    key = f.read()
+                    #create cipher block for decryption
+                    privkey = RSA.import_key(key)
+                    cipher_dec = PKCS1_OAEP.new(privkey)
+                #import server public key
+                with open(os.path.join(sys.path[0], "server_public.pem"), "rb") as f:
+                    key = f.read()
+                    #create cipher block for encryption
+                    pubkey = RSA.import_key(key)
+                    cipher_enc = PKCS1_OAEP.new(pubkey)
 
-            #for parent prcess
-            pass
+                #Receive client message and decrypt message
+                encrypted_message = connectionSocket.recv(2048)
+                decrypted_message = cipher_dec.decrypt(encrypted_message).decode('ascii')
+                decrypted_data = decrypted_message.split(" ")
+                username = decrypted_data[0]
+                password = decrypted_data[1]
+
+                #Authenticate users
+                counter = 0
+                with open(os.path.join(sys.path[0], "user_pass.json"), "rb") as json_file:
+                    user_pass = json.load(json_file)
+                for user, pw in user_pass.items():
+                    if (user == username and pw == password):
+                        counter += 1
+                        break
+                #If there is a match, send menu
+                if (counter == 1):
+                    #generate sym pw and send to client
+                    KeyLen = 256
+                    sym_key = get_random_bytes(int(KeyLen/8))
+                    encrypted_key = cipher_enc.encrypt(sym_key.encode('ascii'))
+                    connectionSocket.send(encrypted_key)
+                    #print on server screen
+                    print("Connection Accepted and Symmetric Key Generated for client: username")
+                else:
+                    #send error message
+                    connectionSocket.send("Invalid username or password".encode('ascii'))
+                    #print error message to server
+                    print("The received client information: " + username + " is invalid (Connection Terminated).")
+                    #terminate connection
+                    connectionSocket.close()
+                    return
+
+                #Generate ciphering block for symm key
+                
+
+                #Receive client response and send menu
+                encrypted_response = connectionSocket.recv(2048)
+                
+
+            # 2: For parent process
+            else:
+                connectionSocket.close()
+            
 
 
-
+        #If error occurred during connection with client
         except socket.error as e:
             print("an error occurred while connecting: ", e)
             serverSocket.close()
